bf6afaaa5c62d034b3d9698535eac9f38b76fc40
Jem Raymundo <jeremuel.raymundo@nrnssoftware.com.au>
2015-09-08 18:07:58 +0800

    Add breadcrumbns to admin deonomination

--- app/Http/breadcrumbs.php
+++ app/Http/breadcrumbs.php
@@ -9,5 +9,22 @@ Breadcrumbs::register('service.index', function ($breadcrumbs) {
 Breadcrumbs::register('service.create', function ($breadcrumbs) {
     $breadcrumbs->parent('service.index');
     $breadcrumbs->push('Create Income', route('service.create'), ['test' => 'Create']);
+});
 
+
+// Admin Denominations
+Breadcrumbs::register('admin.denominations.index', function ($breadcrumbs) {
+    $breadcrumbs->push('List', route('admin.denominations.index'));
+});
+
+// Admin Denominations > Create New Denomination
+Breadcrumbs::register('admin.denominations.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('admin.denominations.index');
+    $breadcrumbs->push('Create New Denomination', route('admin.denominations.create'));
+});
+
+// Admin Denominations > Update Denomination
+Breadcrumbs::register('admin.denominations.edit', function ($breadcrumbs) {
+    $breadcrumbs->parent('admin.denominations.index');
+    $breadcrumbs->push('Edit Denomination', route('admin.denominations.edit'));
 });
