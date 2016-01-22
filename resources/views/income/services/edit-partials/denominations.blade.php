fa08db9f4737433dee13cad03c6451a7a8ead933
Jem Raymundo <jeremuel.raymundo@nrnssoftware.com.au>
2016-01-22 15:53:04 +0800

    Create Income Service Denomination and Technical Debt
    Create and Loads Income Service Denomination
    Sum total for each row.
    
    Technical Debt Remove angularjs code from the the html
    Resolves: Ticket#61 & 59

--- resources/views/income/services/edit-partials/denominations.blade.php
+++ resources/views/income/services/edit-partials/denominations.blade.php
@@ -1,51 +1,49 @@
 {!! Form::open(['class' => 'form-horizontal']) !!}
-<div class="form-group">
-    <label class="col-sm-2 control-label">10</label>
 
-    <div class="col-sm-10">
-        {!! Form::text('tens', null, ['class' => 'form-control text-right input-sm', 'placeholder' => 'Tens']) !!}
-    </div>
-</div>
-<div class="form-group">
-    <label class="col-sm-2 control-label">20</label>
+<div class="wrapper wrapper-content animated fadeInRight">
 
-    <div class="col-sm-10">
-        {!! Form::text('twentys', null, ['class' => 'form-control text-right input-sm', 'placeholder' => 'Twentys']) !!}
-    </div>
-</div>
-<div class="form-group">
-    <label class="col-sm-2 control-label">50</label>
+    <div class="col-lg-4">
 
-    <div class="col-sm-10">
-        {!! Form::text('fiftys', null, ['class' => 'form-control text-right input-sm', 'placeholder' => 'Fiftys']) !!}
-    </div>
-</div>
-<div class="form-group">
-    <label class="col-sm-2 control-label">100</label>
+        <table class="table table-hover no-margins">
+            <thead>
+            <tr>
+                <th>Amount</th>
+                <th>Piece</th>
+                <th class="text-right">Total</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr ng-repeat="(key, denomination) in incomeService.denominations_structure">
+                <td><% denomination.amount %></td>
+                <td>
+                    <input class="form-control text-right input-sm" type="text" value="<%denomination.total%>"
+                           name="denomination_<% denomination.id %>"
+                           ng-keyup="
+                           totalPerDenomination =  piece * denomination.amount;
+                           updateDenominationObject(key, piece, 'piece');
+                           updateDenominationObject(key, totalPerDenomination, 'total');
+                           sumDenomination()"
+                           ng-init="totalPerDenomination=denomination.total; piece = denomination.piece; sumDenomination()"
+                           ng-model="piece">
+                <td class="text-right"><h3><%totalPerDenomination | number%></h3></td>
+            </tr>
+            <tr>
+                <th colspan="3" class="text-right">
+                    <h1 class="no-margins"><%denominationTotal | number%></h1>
+                    <small>Total Income</small>
+                </th>
+            </tr>
+            </tbody>
+        </table>
 
-    <div class="col-sm-10">
-        {!! Form::text('on_hundreds', null, ['class' => 'form-control text-right input-sm', 'placeholder' => 'One Hundreds']) !!}
-    </div>
-</div>
-<div class="form-group">
-    <label class="col-sm-2 control-label">200</label>
+        <hr>
+        <div class="col-lg-12">
+            <button class="btn btn-success btn-block" type="button" id="addMemberToListBtn">Accept Total</button>
+        </div>
 
-    <div class="col-sm-10">
-        {!! Form::text('two_hundreds', null, ['class' => 'form-control text-right input-sm', 'placeholder' => 'Two Hundreds']) !!}
     </div>
-</div>
-<div class="form-group">
-    <label class="col-sm-2 control-label">500</label>
 
-    <div class="col-sm-10">
-        {!! Form::text('five_hundreds', null, ['class' => 'form-control text-right input-sm', 'placeholder' => 'Five Hundreds']) !!}
-    </div>
-</div>
-<div class="form-group">
-    <label class="col-sm-2 control-label">1000</label>
 
-    <div class="col-sm-10">
-        {!! Form::text('one_thousands', null, ['class' => 'form-control text-right input-sm', 'placeholder' => 'One Thousands']) !!}
-    </div>
 </div>
+
 {!! Form::close() !!}
--- resources/views/income/services/edit.blade.php
+++ resources/views/income/services/edit.blade.php
@@ -18,23 +18,13 @@
                             <div id="tab-1" class="tab-pane active">
                                 <div class="panel-body">
                                     <div class="row">
-                                        <div class="col-sm-12">
-                                            <!--@include('income.services.edit-partials.totals')-->
-                                        </div>
-                                    </div>
-                                    <hr/>
-
-                                    <div class="row">
                                         <div class="col-lg-3">
 
                                             {!! Form::open(['class' => 'form-horizontal', 'id' => 'fundStructureForm']) !!}
 
-
                                             <h5 class="font-bold text-muted">Search Member</h5>
 
                                             <div class="form-group ">
-
-
                                                 <div class="col-md-12">
                                                     <input type="text"
                                                            autocomplete="off"
@@ -78,7 +68,6 @@
 
                                         </div>
 
-
                                         <div class="col-lg-9">
                                             <div class="scroll_content" style="overflow-y: scroll;">
                                                 <div class="table-responsive">
@@ -103,223 +92,11 @@
         </div>
     </div>
 
-
-
 @endsection
 
-
 @section('module-scripts')
-
     <script type="text/javascript">
-
-        var incomeService = angular.module('incomeService', ['ui.bootstrap'], function ($interpolateProvider) {
-            $interpolateProvider.startSymbol('<%');
-            $interpolateProvider.endSymbol('%>');
-        });
-
-        incomeService.controller('IncomeServiceCtrl', function ($scope, $http) {
-            $scope.incomeServiceId = parseInt({!! $id !!});
-            $scope.incomeService = {};
-            $scope.members = {};
-            $scope.selectedMember = '';
-            $scope.showMinistriesMemberFund = false;
-            $scope.totalTithes = 0;
-            $scope.totalOffering = 0;
-            $scope.totalOtherFund = 0;
-            $scope.totalFunds = 0;
-
-            $scope.setIncomeService = function () {
-                $http({
-                    method: 'GET',
-                    url: BASE + 'income-services/' + $scope.incomeServiceId,
-                    headers: {'Authorization': 'Bearer ' + localStorage.getItem('userToken')}
-                }).success(function (data, status) {
-
-                    $scope.incomeService = data.IncomeService[0];
-                    $scope.setUpValidation($scope.incomeService.funds_structure); //Set-Up validation on page load
-                    $scope.setTotals($scope.incomeService);
-
-                }).error(function (data, status) {
-                    if (status == 401) {
-                        alert('@todo create a 404 page')
-                    }
-                })
-            };
-
-            $scope.getFieldsForValidation = function (fundStructure) {
-
-                var ruleSet = {selectedMember: {required: true}};
-                var fund = [];
-
-                angular.forEach(fundStructure, function (fund, key) {
-
-                    var item = [];
-
-                    angular.forEach(fund.item, function (item, key) {
-                        ruleSet[item.name] = {number: true};
-                    }, item);
-                }, fund);
-
-                return ruleSet;
-            };
-
-            $scope.getMembers = function () {
-                $http({
-                    method: 'GET',
-                    url: BASE + 'members',
-                    headers: {'Authorization': 'Bearer ' + localStorage.getItem('userToken')}
-                }).success(function (data, status) {
-                    $scope.members = data;
-                }).error(function (data, status) {
-                    alert('Failed to load! Refresh page!')
-                })
-            };
-
-            $scope.addMemberToList = function () {
-                var fundStructure = $scope.incomeService.funds_structure;
-                var fundStructureInput = this.getFundStructureInput(fundStructure);
-                $scope.save(fundStructureInput);
-            };
-
-            $scope.save = function (fundStructureInput) {
-
-                $http({
-                    method: 'POST',
-                    data: fundStructureInput,
-                    url: BASE + 'income-services/' + $scope.incomeServiceId + '/member-fund/' + $scope.selectedMember.id + '/update',
-                    dataType: 'json',
-                    headers: {'Authorization': 'Bearer ' + localStorage.getItem('userToken')}
-                }).success(function (data, status) {
-
-                    //add selected member to push payload
-                    data.memberFundTotal.member = $scope.selectedMember.fullname;
-
-                    //push payload to member list
-                    $scope.incomeService.member_fund_total.push(data.memberFundTotal);
-
-                    //set Total
-                    $scope.setTotals(data.fundTotal);
-
-                    //clear fields
-                    $scope.clearFields();
-
-                }).error(function (data, status) {
-                    if (status == 422) {
-                        if (data.errors.hasOwnProperty('member_id')) {
-                            $scope.selectedMember = '';
-                            angular.element('[name=selectedMember]').addClass('error').focus();
-                            angular.element('#selectedMember-error').show().text('Member does not exists.');
-                        }
-                    }
-                });
-
-            };
-
-            $scope.getFundStructureInput = function (fundStructure) {
-
-                var memberFunds = [];
-                var fund = [];
-
-                angular.forEach(fundStructure, function (fund, key) {
-
-                    var item = [];
-
-                    angular.forEach(fund.item, function (item, key) {
-                        memberFunds.push({
-                            income_service_id: $scope.incomeServiceId,
-                            member_id: $scope.selectedMember.id,
-                            fund_id: fund.id,
-                            fund_item_id: item.fund_item_id,
-                            amount: parseFloat(item.amount),
-                            created_at: '<?php echo date("Y-m-d H:i:s");?>',
-                            updated_at: '<?php echo date("Y-m-d H:i:s");?>'
-                        });
-                    }, item);
-
-                }, fund);
-
-                return memberFunds;
-            };
-
-            $scope.clearFields = function () {
-
-                var fund = [];
-
-                angular.forEach($scope.incomeService.funds_structure, function (fund, key) {
-
-                    var item = [];
-
-                    angular.forEach(fund.item, function (item, key) {
-                        item.amount = 0;
-                    }, item);
-
-                }, fund);
-
-                $scope.selectedMember = '';
-            };
-
-            $scope.removeMember = function (member) {
-
-                var incomeServiceId = member.income_service_id;
-                var memberId = member.member_id;
-
-                swal({
-                    title: "Are you sure?",
-                    text: "You will have to input the data again if you wish to add this person.",
-                    type: "warning",
-                    showCancelButton: true,
-                    confirmButtonColor: "#DD6B55",
-                    confirmButtonText: "Yes, remove it!",
-                    closeOnConfirm: false
-                }, function () {
-
-                    $http({
-                        method: 'DELETE',
-                        url: BASE + 'income-services/' + incomeServiceId + '/member-fund/' + memberId,
-                        dataType: 'json',
-                        headers: {'Authorization': 'Bearer ' + localStorage.getItem('userToken')}
-                    }).success(function (data, status) {
-
-                        //remove member from the list
-                        $scope.incomeService.member_fund_total.splice(member, 1);
-
-                        //set Total
-                        $scope.setTotals(data.fundTotal);
-
-                        swal("Deleted!", "", "success");
-
-                    }).error(function (data, status) {
-                    });
-                });
-            };
-
-            $scope.setUpValidation = function (fundStructure) {
-                angular.element('#fundStructureForm').validate({
-
-                    rules: $scope.getFieldsForValidation(fundStructure),
-
-                    submitHandler: function (form) {
-                        angular.element('#addMemberToListBtn').trigger('click');
-                    }
-                });
-            };
-
-            $scope.setTotals = function (incomeService) {
-                $scope.totalTithes = incomeService.tithes;
-                $scope.totalOffering = incomeService.offering;
-                $scope.totalOtherFund = incomeService.other_fund;
-                $scope.totalFunds = incomeService.total;
-            };
-
-            $scope.toggleMinistriesMemberFund = function (toggleValue) {
-                $scope.showMinistriesMemberFund = (!toggleValue) ? false : true;
-            };
-
-            $scope.setIncomeService();
-            $scope.getMembers();
-
-        });
-
-
+        var incomeServiceId = parseInt({!!$id!!});
     </script>
+    {!! Html::script('js/modules/service/income-edit.js') !!}
 @endsection
