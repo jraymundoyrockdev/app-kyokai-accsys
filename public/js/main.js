7f67107b0c163bbf0f31182d3737816192960b70
Jem Raymundo <jraymundo.yrockdev@gmail.com>
2016-06-10 17:49:52 +0800

    User-Navigation-Logouot

--- public/js/main.js
+++ public/js/main.js
@@ -6,11 +6,15 @@ var kyokaiNavigation = angular.module('kyokaiNavigation', [], function ($interpo
 kyokaiNavigation.controller('kyokaiNavigationCtrl', function ($scope, $http) {
 
     $scope.moduleList = {};
+    $scope.userLoggedIn = '';
 
     $scope.setNavigation = function () {
+        setNavigationModules();
+        setUserLoggedIn();
+    };
 
+    function setNavigationModules() {
         var modules = JSON.parse(localStorage.getItem('modules'));
-
         var moduleList = [];
 
         for (var prop in modules) {
@@ -18,14 +22,18 @@ kyokaiNavigation.controller('kyokaiNavigationCtrl', function ($scope, $http) {
         }
 
         $scope.moduleList = modules;
-    };
+    }
+
+    function setUserLoggedIn() {
+        $scope.userLoggedIn = localStorage.getItem('username');
+    }
 });
 
 kyokaiNavigation.filter('isEmpty', [function () {
     return function (object) {
         return angular.equals({}, object);
     }
-}])
+}]);
 
 
 var kyokaiHeader = angular.module('kyokaiHeader', [], function ($interpolateProvider) {
@@ -36,7 +44,7 @@ var kyokaiHeader = angular.module('kyokaiHeader', [], function ($interpolateProv
 kyokaiHeader.controller('kyokaiHeaderCtrl', function ($scope, $http) {
     $scope.isKyokaiAccountant = false;
 
-    $scope.setHeader = function(){
+    $scope.setHeader = function () {
         $scope.isKyokaiAccountant = (localStorage.getItem('isKyokaiAccountant') == 'true');
     }
 });
--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -10,7 +10,7 @@
                         <a data-toggle="dropdown" class="dropdown-toggle" href="#">
                             <span class="clear">
                                 <span class="block m-t-xs">
-                                    <strong class="font-bold">DavidWilliams</strong>
+                                    <strong class="font-bold"><%userLoggedIn%></strong>
                                 </span>
                                 <span class="text-muted text-xs block">
                                     Art Director <b class="caret"></b>
